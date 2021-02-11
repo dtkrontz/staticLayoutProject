3f677bcda6b33969fec74b9cec16b973ea027165
Dtkrontz <dtkrontz@gmail.com>
2021-02-11 17:53:26 -0500

    02/11/2021 third commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function dropDown () {
+    document.getElementsByClassName('.dropBtn').classList.toggle('show');
+}
 
+window.onclick = function(event) {
+    if (!event.target.matches('.dropBtn')) {
+      let dropdowns = document.getElementsByClassName("dropDownContent");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
